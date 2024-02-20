85d413a0cc1bc43b40089ed5be7f62a7613a77e3
Azizbek Isroilov <aziz16110904@gmail.com>
2024-02-20 15:36:41 +0500

    remove log

--- app/Http/Controllers/PaymeController.php
+++ app/Http/Controllers/PaymeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class PaymeController extends Controller
