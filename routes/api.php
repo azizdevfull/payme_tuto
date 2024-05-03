0cbc733a884896063c37e7d9a5073560f9b5776b
Azizbek Isroilov <aziz16110904@gmail.com>
2024-05-03 15:47:09 +0500

    completed payme middleware

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ use App\Http\Controllers\ProfileController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/payme', [PaymeController::class, 'index']);
+Route::post('/payme', [PaymeController::class, 'index'])->middleware('payme');
 Route::post('/payme-profile', [ProfileController::class, 'index']);
